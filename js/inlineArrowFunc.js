66bef7ed1e34285439f3b9c89441a4392d2ea224
tsuem <tsuemotop@gmail.com>
2022-09-18 17:59:17 -0300

    Inline arrow functions

---
+++ js/inlineArrowFunc.js
@@ -0,0 +1,9 @@
+//ejemplo
+/* const showText = () => {
+    return 'Hola Mundo'
+} */
+
+const showText = () => 'Hola Mundo'
+const showNumber = () => 25
+const showBoolean = () => true
+const showArray = () => [1, 2, 3]
