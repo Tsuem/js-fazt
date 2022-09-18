754beacac3e6584455feba40d87c2f4bba507e7c
tsuem <tsuemotop@gmail.com>
2022-09-18 00:35:50 -0300

    functions

---
+++ index.js
@@ -0,0 +1,28 @@
+const hello = () => {
+    console.log('hola mundo');
+}  
+
+hello()
+
+/* function hello() {
+    console.log('hola mundo');
+    console.log('trabajando con JS');
+}
+
+hello() */
+
+
+/* function hello() {
+    return 'hola mundo'
+}
+
+console.log(hello()) */
+
+
+/* function hello() {
+    return function() {
+        return 'hola mundo'
+    }
+}
+
+console.log(helo()()); */
